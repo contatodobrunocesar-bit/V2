6afaf358e62d35a40f380d4d2e0863ae3f48228e
contatodobrunocesar-bit <contatodobrunocesar@gmail.com>
2025-09-18 13:30:47 -0300

    Corrigir configuração do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       plugins: [react()],
+      server: {
+        port: 5173,
+        host: true
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
